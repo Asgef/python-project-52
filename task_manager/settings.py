7f99e7a7782761f18621dad4d15698609e847dd4
Asgef <asgefes1@gmail.com>
2024-05-01 15:42:38 +0300

    Update django_bootstrap

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap4',
+    'django_bootstrap5',
     'task_manager',
 ]
 
