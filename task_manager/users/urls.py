2ede87d727f897d31c799140587b8689e595e717
Asgef <asgefes1@gmail.com>
2024-05-05 18:13:20 +0300

    Add user registration

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from task_manager.users.views import UsersListView
+from task_manager.users.views import UsersListView, UserRegisterView
 
 
 urlpatterns = [
     path('', UsersListView.as_view(), name='users'),
+    path('create/', UserRegisterView.as_view(), name='create')
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.contrib.messages.views import SuccessMessageMixin
+from .forms import UserForm
 from .models import User
+from django.urls import reverse_lazy
 
 
 class UsersListView(ListView):
@@ -10,3 +13,15 @@ class UsersListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+
+class UserRegisterView(SuccessMessageMixin, CreateView):
+    template_name = 'users/user_form.html'
+    form_class = UserForm
+    model = User
+    success_url = reverse_lazy('home_page')
+    success_message = 'User is successfully registered'
+    extra_context = {
+        'title': 'Create user',
+        'button_text': 'Register',
+    }
