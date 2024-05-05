6b77cc1dea6d39c70231016a6f93e2760bd7f8f1
Asgef <asgefes1@gmail.com>
2024-05-05 21:15:23 +0300

    Refactor HomePageView

--- task_manager/views.py
+++ task_manager/views.py
@@ -5,10 +5,21 @@ from django.utils.translation import gettext as _
 class HomePageView(TemplateView):
 
     template_name = "index.html"
+    extra_context = {
+        'title': _('Task Manager Hexlet'),
+        'greeting': _('Hello from Hexlet!'),
+        'description': _('Practical programming courses'),
+        'invitation_button': _('Learn more')
+    }
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['greeting'] = _('Hello from Hexlet!')
-        context['description'] = _('Practical programming courses')
-        context['invitation_button'] = _('Learn more')
-        return context
+
+# class UserRegisterView(SuccessMessageMixin, CreateView):
+#     template_name = 'users/user_form.html'
+#     form_class = UserForm
+#     model = User
+#     success_url = reverse_lazy('home_page')
+#     success_message = 'User is successfully registered'
+#     extra_context = {
+#         'title': 'Registration',
+#         'button_text': 'Register',
+#     }
