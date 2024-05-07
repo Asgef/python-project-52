2cac8000f0760d3bad995b38c303815ef1228ae4
Asgef <asgefes1@gmail.com>
2024-05-07 13:14:56 +0300

    Minor fix

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -10,7 +10,6 @@ from django.contrib import messages
 
 class UsersListView(ListView):
     model = User
-    paginate_by = 20
     template_name = 'users/users.html'
 
     def get_context_data(self, **kwargs):
