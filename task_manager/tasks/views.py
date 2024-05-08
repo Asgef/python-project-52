506c1fd8b6cde1a31ffcb45750d913a4a8ea7c99
Asgef <asgefes1@gmail.com>
2024-05-09 00:03:20 +0300

    Fix lint

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,10 +1,10 @@
-from task_manager.mixins import AuthRequiredMixin, DeleteProtectionMixin
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from task_manager.mixins import AuthRequiredMixin  # DeleteProtectionMixin
+from django.views.generic import ListView  # CreateView, UpdateView, DeleteView
 from django.utils.translation import gettext_lazy as _
 from .models import Task
-from .forms import TaskForm
-from django.contrib.messages.views import SuccessMessageMixin
-from django.urls import reverse_lazy
+# from .forms import TaskForm
+# from django.contrib.messages.views import SuccessMessageMixin
+# from django.urls import reverse_lazy
 
 
 class TasksListView(AuthRequiredMixin, ListView):
