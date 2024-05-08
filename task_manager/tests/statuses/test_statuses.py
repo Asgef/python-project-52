ada04e443145947d7bbe895e92ef8f685c0c2dd1
Asgef <asgefes1@gmail.com>
2024-05-08 18:48:43 +0300

    Fix test for "statuses"

--- task_manager/tests/statuses/test_statuses.py
+++ task_manager/tests/statuses/test_statuses.py
@@ -1,74 +1,64 @@
-# from task_manager.users.models import User
-# from django.urls import reverse_lazy
-# from django.test import TestCase
-# from task_manager.statuses.models import Status
-#
-#
-# class Create(TestCase):
-#     fixtures = ['status.json']
-#
-#     def test_open_without_login(self):
-#         response = self.client.get(reverse_lazy('status_add'))
-#         self.assertEqual(response.status_code, 302)
-#
-#     def test_create_status(self):
-#         user = User.objects.get(pk=1)
-#         self.client.force_login(user=user)
-#         statuses = Status.objects.all()
-#         response = self.client.get(reverse_lazy('status_add'))
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(len(statuses), 1)
-#         response = self.client.post(
-#             reverse_lazy('status_add'),
-#             {'name': 'test'}
-#         )
-#         statuses_2 = Status.objects.all()
-#         self.assertEqual(len(statuses_2), 2)
-#         status_added = statuses_2[1]
-#         self.assertEqual(status_added.name, 'test')
-#
-#
-# class UpdateStatus(TestCase):
-#     fixtures = ['status.json']
+from task_manager.users.models import User
+from django.urls import reverse_lazy
+from django.test import TestCase
+from task_manager.statuses.models import Status
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class StatusTestCase(TestCase):
+    fixtures = ['status.json']
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+        self.client.force_login(self.user)
+
+
+class StatusAddViewTest(StatusTestCase):
+
+    def test_open_without_login(self):
+        self.client.logout()
+        response = self.client.get(reverse_lazy('statuses'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_create_status(self):
+        initial_count = Status.objects.count()
+        response = self.client.post(reverse_lazy('status_add'), {'name': 'New Status'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Status.objects.count(), initial_count + 1)
+        new_status = Status.objects.latest('id')
+        self.assertEqual(new_status.name, 'New Status')
+
+
+# class UpdateStatus(StatusTestCase):
 #
 #     def test_update_without_login(self):
-#         response = self.client.get(
-#             reverse_lazy('status_update', kwargs={'pk': 1})
-#         )
+#         self.client.logout()
+#         response = self.client.get(reverse_lazy('status_update', kwargs={'pk': 1}))
 #         self.assertEqual(response.status_code, 302)
 #
 #     def test_update_status(self):
-#         user = User.objects.get(pk=1)
-#         self.client.force_login(user=user)
-#         response = self.client.get(
-#             reverse_lazy('status_update', kwargs={'pk': 1})
-#         )
+#         response = self.client.get(reverse_lazy('status_update', kwargs={'pk': 1}))
 #         self.assertEqual(response.status_code, 200)
 #         response = self.client.post(
-#             reverse_lazy('status_update', kwargs={'pk': 1}), {'name': 'test'}
+#             reverse_lazy('status_update', kwargs={'pk': 1}), {'name': 'Updated Status'}
 #         )
-#         status = Status.objects.get(pk=1)
-#         self.assertEqual(status.name, 'test')
+#         self.assertEqual(response.status_code, 302)
+#         updated_status = Status.objects.get(pk=1)
+#         self.assertEqual(updated_status.name, 'Updated Status')
 #
 #
-# class DeleteStatus(TestCase):
-#     fixtures = ['status.json']
+# class DeleteStatus(StatusTestCase):
 #
 #     def test_delete_without_login(self):
-#         response = self.client.get(
-#             reverse_lazy('status_delete', kwargs={'pk': 1})
-#         )
+#         self.client.logout()
+#         response = self.client.get(reverse_lazy('status_delete', kwargs={'pk': 1}))
 #         self.assertEqual(response.status_code, 302)
 #
 #     def test_delete_status(self):
-#         user = User.objects.get(pk=1)
-#         self.client.force_login(user=user)
-#         response = self.client.get(
-#             reverse_lazy('status_delete', kwargs={'pk': 1})
-#         )
-#         self.assertEqual(response.status_code, 200)
-#         response = self.client.post(
-#             reverse_lazy('status_delete', kwargs={'pk': 1})
-#         )
-#         statuses = Status.objects.all()
-#         self.assertEqual(len(statuses), 0)
+#         initial_count = Status.objects.count()
+#         response = self.client.post(reverse_lazy('status_delete', kwargs={'pk': 1}))
+#         self.assertEqual(response.status_code, 302)
+#         self.assertEqual(Status.objects.count(), initial_count - 1)
